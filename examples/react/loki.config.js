194879db67f51ae3333931e154d5b60347751a59
Anxhul10 <anxhul10@gmail.com>
2025-06-12 14:37:16 +0530

    fix: add url to fetchFailIgnore

--- examples/react/loki.config.js
+++ examples/react/loki.config.js
@@ -7,14 +7,6 @@ module.exports = {
       width: 1366,
       height: 768,
     },
-    'chrome.iphone7': {
-      target: 'chrome.docker',
-      preset: 'iPhone 7',
-    },
-    'chrome.a4': {
-      target: 'chrome.docker',
-      preset: 'A4 Paper',
-    },
   },
-  fetchFailIgnore: 'localhost:1234/get',
+  fetchFailIgnore: 'localhost:1234/get|192\\.168\\.29\\.108:6006/runtime-error',
 };
